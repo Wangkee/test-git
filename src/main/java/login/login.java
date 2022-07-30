fc106538a4cb672619e00d2c481b5c048597009d
wangke <wangkee2002@gmail.com>
2022-07-30 15:25:04 +0800

    "b1"

--- src/main/java/login/login.java
+++ src/main/java/login/login.java
@@ -4,5 +4,6 @@ public class login {
     public static void main(String[] args) {
         System.out.println("login");
         System.out.println("b1");
+        System.out.println("b1");
     }
 }
